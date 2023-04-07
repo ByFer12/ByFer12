cd2fd1c793a039c338025e28c11fffa996089119
Byron Torres <ajxupbyron@gmail.com>
2023-04-07 12:50:28 -0600

    Proyecto llamar funcion OPenbootcamp

---
+++ src/main/java/demo/App.java
@@ -0,0 +1,20 @@
+package demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ){
+
+       int suma= suma(12, 20, 18);
+        System.out.println("La suma es: "+suma);
+
+    }
+
+    public static int suma(int num1, int num2, int num3){
+        
+        return num1+num2+num3;
+    }
+}
